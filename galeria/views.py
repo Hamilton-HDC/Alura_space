6248829a94831007c95ae02c43ee9bd7566a97a4
Hamilton Cesar <hamiltondc.dc@gmail.com>
2025-03-28 03:00:08 -0300

    Acrescentando data e hora no banco para quando for alterado

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
+
 def index(request):
-    fotografias = Fotografia.objects.all()
+    fotografias = Fotografia.objects.filter(publicada=True)
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
 
