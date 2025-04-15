b555e62b7ecd2f4f6eff0ad558fad12f569cd80f
Hamilton Cesar <hamiltondc.dc@gmail.com>
2025-04-15 02:15:59 -0300

    Dando continuidade ao projeto

--- setup/urls.py
+++ setup/urls.py
@@ -26,5 +26,5 @@ Including another URLconf
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('galeria.urls')),
-    path('', include('usuarios.urls')),
+    path('usuarios/', include('usuarios.urls')),
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT )
